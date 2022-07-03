b7d0793ec8ccb44a225ce250d171589e0709f923
hagit-aharonof <kfir7397@gmail.com>
2022-07-03 18:00:31 +0300

    add doc viewer

--- JS/HomePageJS.js
+++ JS/HomePageJS.js
@@ -13,3 +13,22 @@ function scrolltotop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// https://drive.google.com/file/d/17FvLqWiLliDzjT5Lv3mE1Le4WTbfqLaF/preview
+function create_doc_viewer(pdf_file) {
+  var new_win = open('https://hagit-aharonof.github.io/WebPages/DocViewer.html','pdf viewer');
+  html_to_write = `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Document</title>
+                    </head>
+                    <body>
+                        <iframe src="` + pdf_file + `" width="100%" height="1000" allow="autoplay"></iframe>
+                    </body>
+                    </html>`
+  new_win.document.write(html_to_write);
+}
